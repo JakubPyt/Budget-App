c37a35eb764ef8aff4dd3f715ffcc5f66bb3f896
JakubPyt <pyt.jakub@gmail.com>
2021-05-03 18:30:21 +0200

    notebook/balance/Display header_lbl and warnings_lbl

--- notebook_balance.py
+++ notebook_balance.py
@@ -1,26 +1,38 @@
 from tkinter import ttk
-
+import tkinter as tk
 
 class BalanceView:
     def __init__(self, notebook, ledger):
         # Get main frame to work in it
         self.notebook = notebook
+
         # Get instance of class ledger
         self.ledger = ledger
 
-    def display(self):
         # Create self main frame
-        balance_frame = ttk.Frame(
+        self.balance_frame = ttk.Frame(
             master=self.notebook
         )
-        balance_frame.pack(fill='both', expand=True)
+        self.balance_frame.pack(fill='both', expand=True)
 
+    def display(self):
         # Content of the main frame
-        welcome_lbl = ttk.Label(
-            master=balance_frame,
-            text="Hello from balance"
+        # Header
+        header_lbl = ttk.Label(
+            master=self.balance_frame,
+            text="Your ledger",
+            font=("Courier", 24, 'bold'),
         )
-        welcome_lbl.pack()
+        header_lbl.pack(pady=20)
+        # Place for warnings
+        self.warnings_lbl = ttk.Label(
+            master=self.balance_frame,
+            text='',
+            foreground='red'
+        )
+        self.warnings_lbl.pack()
+
 
         # Return main frame
-        return balance_frame
+        return self.balance_frame
+
