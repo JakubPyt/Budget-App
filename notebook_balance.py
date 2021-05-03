d98473ca725a2a311b1866daee4644cbc575a43a
JakubPyt <pyt.jakub@gmail.com>
2021-05-03 18:35:56 +0200

    notebook/balance/Display add_withdraw_section

--- notebook_balance.py
+++ notebook_balance.py
@@ -63,7 +63,9 @@ class BalanceView:
             height=250
         )
         add_deposit_section = self.print_add_deposit(nb)
+        add_withdraw_section = self.print_add_withdraw(nb)
         nb.add(add_deposit_section, text="Add deposit")
+        nb.add(add_withdraw_section, text="Add withdraw")
         nb.pack(side=tk.LEFT, anchor=tk.N, padx=50, pady=10)
 
     # Functions for adding a deposit
@@ -122,4 +124,60 @@ class BalanceView:
         self.print_table()
         self.add_deposit_amount_entry.delete(0, 'end')
         self.add_deposit_desc_entry.delete(0, 'end')
+
+    # Functions for adding a withdraw
+    def print_add_withdraw(self, notebook):
+        # Frame
+        add_withdraw_frame = ttk.Frame(notebook)
+        add_withdraw_frame.pack(fill='both', expand=True)
+
+        # Amount
+        add_withdraw_amount_lbl = ttk.Label(
+            master=add_withdraw_frame,
+            text="Amount:"
+        )
+        add_withdraw_amount_lbl.pack(pady=10)
+        self.withdraw_amount = tk.StringVar()
+        self.add_withdraw_amount_entry = ttk.Entry(
+            master=add_withdraw_frame,
+            textvariable=self.withdraw_amount
+        )
+        self.add_withdraw_amount_entry.pack()
+
+        # Category
+        add_withdraw_category_lbl = ttk.Label(
+            master=add_withdraw_frame,
+            text="Category:"
+        )
+        add_withdraw_category_lbl.pack(pady=10)
+        self.options = ['Unsigned', 'Car', 'Food', 'Child', 'Taxes', 'Unsigned']
+        self.withdraw_category = tk.StringVar()
+        add_withdraw_category_om = ttk.OptionMenu(
+            add_withdraw_frame,
+            self.withdraw_category,
+            *self.options
+        )
+        add_withdraw_category_om.pack()
+        # Description
+        add_withdraw_desc_lbl = ttk.Label(
+            master=add_withdraw_frame,
+            text='Description:'
+        )
+        add_withdraw_desc_lbl.pack(pady=10)
+        self.withdraw_desc = tk.StringVar()
+        self.add_withdraw_desc_entry = ttk.Entry(
+            master=add_withdraw_frame,
+            textvariable=self.withdraw_desc
+        )
+        self.add_withdraw_desc_entry.pack()
+
+        # Submit
+        add_withdraw_btn = ttk.Button(
+            master=add_withdraw_frame,
+            text="Add...",
+            command=self.add_withdraw
+        )
+        add_withdraw_btn.pack(pady=10)
+
+        return add_withdraw_frame
 
