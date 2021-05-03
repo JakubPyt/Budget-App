027df0fc954f06f058e2e0e875564f0f78790866
JakubPyt <pyt.jakub@gmail.com>
2021-05-03 15:06:54 +0200

    ledger/Create add_deposit func

--- ledger.py
+++ ledger.py
@@ -6,3 +6,12 @@ class Ledger:
         # Create empty ledger to storage information about balance
         self.ledger = pd.DataFrame(columns=['Amount', 'Balance', 'Description', 'Category'])
 
+    # Func add deposit to ledger
+    def add_deposit(self, amount, description):
+        self.ledger = self.ledger.append({
+            'Amount': round(amount, 2),
+            'Balance': (self.ledger['Amount'].sum() + round(amount, 2)),
+            'Description': description,
+            'Category': 'Deposit',
+        }, ignore_index=True)
+
