089e0afb6494c0866e6e1f0db977246f96400f16
JakubPyt <pyt.jakub@gmail.com>
2021-05-04 15:29:14 +0200

    notebook/plots/Update display func
    Update display function to check length of ledger and display right screen

--- notebook_plots.py
+++ notebook_plots.py
@@ -1,26 +1,43 @@
 from tkinter import ttk
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class PlotsView:
     def __init__(self, notebook, ledger):
         # Get main frame to work in it
         self.notebook = notebook
+
         # Get instance of class ledger
         self.ledger = ledger
 
-    def display(self):
         # Create self main frame
-        plots_frame = ttk.Frame(
+        self.plots_frame = ttk.Frame(
             master=self.notebook
         )
-        plots_frame.pack(fill='both', expand=True)
+        self.plots_frame.pack(fill='both', expand=True)
 
+    def display(self):
         # Content of the main frame
-        welcome_lbl = ttk.Label(
-            master=plots_frame,
-            text="Hello from plots"
+        header_lbl = ttk.Label(
+            master=self.plots_frame,
+            text="Visualization of finances",
+            font=("Courier", 24, 'bold'),
         )
-        welcome_lbl.pack()
+        header_lbl.pack(pady=20)
+
+        # Check if ledger is empty
+        if(len(self.ledger.ledger) == 0):
+            empty_ledger_lbl = ttk.Label(
+                master=self.plots_frame,
+                text=self.ledger.get_ledger()
+            )
+            empty_ledger_lbl.pack()
+        else:
+            self.print_balance_plot()
+            self.print_expenses_by_category_plot()
 
         # Return main frame
-        return plots_frame
+        return self.plots_frame
+
