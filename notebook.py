f2eff835682ffe6ba86ae21a28e820e04c054c58
JakubPyt <pyt.jakub@gmail.com>
2021-05-05 16:21:26 +0200

    code-clearing/Add comments to notebook.py

--- notebook.py
+++ notebook.py
@@ -3,30 +3,48 @@ from notebook_balance import BalanceView
 from notebook_plots import PlotsView
 from ledger import Ledger
 
+
 class NotebookView:
     def __init__(self, window):
-        # Create notebook
+        # ==================
+        # This(main) part of app is a notebook widget
+        # ==================
+        # Creating a notebook which will now be our main frame
+        # This notebook will be sent to classes BalanceView and PlotsView
         notebook = ttk.Notebook(
             master=window,
             width=1000,
             height=500
         )
 
-        # Create one instance of class ledger to sent it to BalanceView and PlotsView
+        # Create one instance of class ledger on which we will operate
+        # This instance will be sent to classes BalanceView and PlotsView
         ledger = Ledger()
 
-        # Create an instance of the class that get the mainframe to work in it
+        # Create an instances of the classes to display them as a notebook pages
         bv = BalanceView(notebook, ledger)
+        # We have self below to possibility to send this instance to method "reload"
         self.pv = PlotsView(notebook, ledger)
 
-        # Pages of notebook display main frames from above classes
+        # Add "display" methods from classes as pages
         notebook.add(bv.display(), text="Balance")
         notebook.add(self.pv.display(), text="Plots")
+
+        # Bind notebook to method "reload"
         notebook.bind("<<NotebookTabChanged>>", self.reload)
+
         # Display notebook
         notebook.pack()
 
     def reload(self, event):
+        """
+        Method causes reload page "Plots".
+        """
+        # Check which tab is opened
         tab = event.widget.tab('current')['text']
+        # If tab "Plots" is opened...
         if tab == 'Plots':
-            self.pv.refresh()
+            # ... Call class method "display_content"
+            # This method causes hide current screen with plots
+            # And display plots with new data
+            self.pv.display_content()
